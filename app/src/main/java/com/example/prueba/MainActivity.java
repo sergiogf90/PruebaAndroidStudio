1766414107d938b619140bec91e733bddff9d1d9
Sergio Gutiérrez-Pola <sergiogf90@educastur.es>
2022-10-06 19:18:58 +0200

    Ejercicio 32 y menu

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.prueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this::onClick);
+    }
+
+
+    public void onClick(View v) {
+        Toast.makeText(this,"Botón de prueba",Toast.LENGTH_LONG).show();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.op1:
+                Toast.makeText(this,"Has elegido opcion 1",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.op2:
+                Toast.makeText(this,"Has elegido opcion 2",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.op3:
+                Toast.makeText(this,"Has elegido opcion 3",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        return true;
+    }
+    public void itemClick(MenuItem item){
+        Toast.makeText(this,"Has elegido opcion 3.1",Toast.LENGTH_LONG).show();
     }
 }
